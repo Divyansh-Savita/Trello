aafb3d19cc67611fc3a884a8e82be64a60b2bfb6
Divyansh Savita <savitadivyansh323@gmail.com>
2025-08-23 00:37:15 +0530

    delete feature added

--- Practice/src/App.tsx
+++ Practice/src/App.tsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [items, setItems] = useState<string[]>([]);
+  const [text, setText] = useState("");
+  
+  const addnumber=():void=>{
+    setItems((prev)=>[...prev,text])
+    
+  }
+  const deletenumber=(StrToDelete:String):void=>{
+    setItems(prev=>prev.filter(str=>str!==StrToDelete))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <>
+      <h1>Trello</h1>
+      <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
+      <button onClick={addnumber}>CLick me</button>
+      
+      <ul>
+        {items.map((val,index)=>(
+          <li key={index}>{val} {" "} <button onClick={()=>(deletenumber(val))}>delete</button>
+          </li>
+
+        ))}
+      </ul>
     </>
   )
-}
+};
 
 export default App
